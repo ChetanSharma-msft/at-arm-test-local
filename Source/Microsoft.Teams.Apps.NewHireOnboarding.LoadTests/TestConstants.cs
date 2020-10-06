c354f02d57054c3c3a8a9dc26c602142ce310552
v-chetsh <59052547+v-chetsh@users.noreply.github.com>
2020-10-06 18:57:21 +0530

    Add files via upload

---
+++ Source/Microsoft.Teams.Apps.NewHireOnboarding.LoadTests/TestConstants.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="TestConstants.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.NewHireOnboarding.LoadTests
+{
+    /// <summary>
+    /// A class that holds test settings constants.
+    /// </summary>
+    public class TestConstants
+    {
+        /// <summary>
+        /// View feedback API request URL to fetch feedbacks.
+        /// </summary>
+        public const string ViewFeedbackRequestUrl = "https://www.test.com";
+    }
+}
