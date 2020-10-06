363f492986a91298198bbd4625d2ab990e7590eb
v-chetsh <59052547+v-chetsh@users.noreply.github.com>
2020-10-06 16:37:18 +0530

    Add files via upload

---
+++ Source/Microsoft.Teams.Apps.NewHireOnboarding/DeepLinkConstants.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="DeepLinkConstants.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.NewHireOnboarding
+{
+    /// <summary>
+    /// A class that holds deep links that are used in multiple files.
+    /// </summary>
+    public static class DeepLinkConstants
+    {
+        /// <summary>
+        /// Microsoft Graph API base url.
+        /// </summary>
+        public const string GraphAPIBaseURL = "https://graph.microsoft.com/";
+
+        /// <summary>
+        /// Deep link to navigate to Channels Tab.
+        /// </summary>
+        public const string TabBaseRedirectURL = "https://teams.microsoft.com/l/entity";
+
+        /// <summary>
+        /// Deep link to initiate chat.
+        /// </summary>
+        public const string ChatInitiateURL = "https://teams.microsoft.com/l/chat";
+
+        /// <summary>
+        /// Link that redirects to tab.
+        /// </summary>
+        public const string FeedbackTabURL = "https://teams.microsoft.com/l/entity/{0}/Feedback?context={1}";
+
+        /// <summary>
+        /// Link to open file in teams.
+        /// </summary>
+        public const string OpenFileInTeamsURL = "https://teams.microsoft.com/_#/{0}/viewer/teams/{1}";
+    }
+}
